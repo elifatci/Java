29ff282857db3696135b0ff5cba0a2c2ff649e53
Elif Atçı <eelif0600@gmail.com>
2023-10-08 12:01:23 +0300

    soru

--- src/tekrarlar/soru1.java
+++ src/tekrarlar/soru1.java
@@ -9,16 +9,30 @@ public class soru1 {
 
          */
 
-        for (int i = 1; i <= 5; i++) {
-            for (int j = 1; j <= 5; j++) {
+        int[][] sayi = new int[5][5];
+        int toplam1 = 0;
+        int toplam2 = 0;
+
+        for (int i = 0; i < 5; i++) {
+            for (int j = 0; j < 5; j++) {
                 Random random = new Random();
                 int rastgeleSayi = random.nextInt(9);
+                sayi[i][j] = rastgeleSayi;
                 System.out.print(rastgeleSayi + " ");
 
 
-            }
+                if (i == j) {
+                    toplam1 += sayi[i][j];
+                }
+                if (i+j==4) {
+                    toplam2+=sayi[i][j];
 
+                }
+
+            }
             System.out.println(" ");
         }
+        System.out.println("toplam1: " + toplam1);
+        System.out.println("toplam2: "+ toplam2);
     }
 }
